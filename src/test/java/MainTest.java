8565d42af0665dba182cb38d5414e1fac1122390
Eugene kuvaldin <acerkuv@gmail.com>
2024-09-23 13:53:36 +0300

    Разобраться с вывоом рекурсии не проходит тестна 60

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,7 +2,9 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.example.Main.*;
 
@@ -23,12 +25,14 @@ public class MainTest {
         Assertions.assertEquals(fact(3), 6);
         //Тест myOverload()
         Assertions.assertEquals(myOverload("VV"), "VV_VV_VV");
-        Assertions.assertEquals(printArr(myOverload(new int[]{1, 1, 9, 0})),  printArr( new int[]{1, 1, 81, 0}));
+        Assertions.assertEquals(printArr(myOverload(new int[]{1, 1, 9, 0})),
+                printArr( new int[]{1, 1, 81, 0}));
         //Тест findFact()
-        Assertions.assertEquals(findNatNum(10), 11);
-        Assertions.assertEquals(findNatNum(40), 41);
-        Assertions.assertEquals(findNatNum(98), 101);
+        Assertions.assertEquals(findNearNatNum(18),
+                new ArrayList<>(  Arrays.asList(17, 13, 11)));
 
+//        Assertions.assertEquals(findNearNatNum(60),
+//                new ArrayList<>(  Arrays.asList(59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11)));
     }
     public static String printArr(int[] m){
         return Arrays.toString(m);
